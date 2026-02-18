3801f8eb666a45ce072923575a3e0d0e58b8fc78
JeyaVardhan-L <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-02-18 14:40:29 +0530

    Optimised to chunk the data properly

--- rag_engine.py
+++ rag_engine.py
@@ -11,3 +11,15 @@ def get_embedding(text):
         input=text
     )
     return np.array(res.data[0].embedding)
+def chunk_text(text , size=1200 , overlap=200):
+
+    chunks = []
+    start = 0
+
+    while start < len(text):
+        end = start + size
+        chunk = text[start:end]
+        chunks.append(chunk)
+        start = end - overlap
+
+    return chunks
